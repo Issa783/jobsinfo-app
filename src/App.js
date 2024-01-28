64d7d8e55522a00a482ac16e8287c25181d99357
John Mutua <mutubenedict920@gmail.com>
2024-01-28 11:07:38 +0300

    Added jobsinfo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from "react";
+import { FaAngleRight } from "react-icons/fa";
+import styles from "./style.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [jobs, setJobs] = useState([]);
+    const [value, setValue] = useState(0);
+    const [loading, setLoading] = useState(true);
+  
+    const fetchData = () => {
+      fetch("https://course-api.com/react-tabs-project")
+        .then((res) => res.json())
+        .then((data) => {
+          setJobs(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+        });
+    };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    if (loading) {
+      return (
+        <section className={styles.loading}>
+          <h1>Loading...</h1>
+        </section>
+      );
+    }
+  
+    const { company, dates, duties, title } = jobs[value];
+  
+      return (
+        <section className={styles.section}>
+          <div className={styles.title}>
+           <h2>Experience</h2>
+           <div className={styles.underline}></div>
+         </div>
+          <div className={styles.jobcenter}>
+            <div className={styles.btncontainer}>
+              {jobs.map((item, index) => (
+                <button
+                  key={item.id}
+                  onClick={() => setValue(index)}
+                  className={`${styles.jobbtn} ${
+                    index === value && styles.activebtn
+                  }`}
+                >
+                  {item.company}
+                </button>
+              ))}
+            </div>
+            <article className={styles.jobinfo}>
+              <h3>{title}</h3>
+              <h4>{company}</h4>
+              <p className={styles.jobdate}>{dates}</p>
+              {duties.map((duty, index) => (
+                <div key={index} className={styles.jobdesc}>
+                  <FaAngleRight className={styles.jobicon} />
+                  <p>{duty}</p>
+                </div>
+              ))}
+            </article>
+          </div>
+        </section>
+      );
+    };
 
 export default App;
